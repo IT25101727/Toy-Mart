9406d713815cafd6e6e6435af7cb9c8568b3f885
Yasiru-sahan <it25103666@my.sliit.lk>
2026-05-04 07:37:32 +0530

    refactor(service): CustomerService import bugs fixed

--- Application_Backend/src/main/java/org/com/application/service/custom/CustomerService.java
+++ Application_Backend/src/main/java/org/com/application/service/custom/CustomerService.java
@@ -2,8 +2,8 @@ package org.com.application.service.custom;
 
 
 import org.com.application.dto.DtoCustomer;
-import org.com.application.service.SuperSevice;
+import org.com.application.service.SuperService;
 
-public interface CustomerService extends SuperSevice<DtoCustomer> {
+public interface CustomerService extends SuperService<DtoCustomer> {
     long getCount();
 }

9c5ad921fc3cfb158de5de821ff5876c2da72ad3
Yasiru-sahan <it25103666@my.sliit.lk>
2026-05-13 21:37:39 +0530

    feat(Service):OrderService added

---
+++ Application_Backend/src/main/java/org/com/application/service/custom/OrderService.java
@@ -0,0 +1,9 @@
+package org.com.application.service.custom;
+
+
+import org.com.application.dto.DtoOrder;
+import org.com.application.service.SuperService;
+
+public interface OrderService extends SuperService<DtoOrder> {
+
+}
